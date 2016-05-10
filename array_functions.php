0c797a555fec290b286fa8f322a4e7eaef30ffae
Amirul Islam <amirul.csejust@gmail.com>
2016-05-11 00:58:07 +0600

    array function adding shift array

---
+++ array_functions.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>Array function </title>
+	</head>
+	<body>
+	<h1> hello World</h1>
+	
+	<?php
+	 $number = array(1,2,3,4,5,6);
+	 print_r($number);
+	 
+	 echo "<hr>";
+	 
+	 //shift first element out of an array
+	 $a = array_shift($number);
+	 
+	 print_r($a);
+	 
+	 
+	?>
+	</body>
+
+</html>
