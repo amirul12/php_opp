06cba55586fd6c66d4565d1dfe17c11f04d3c508
Amirul Islam <amirul.csejust@gmail.com>
2016-05-12 16:46:26 +0600

    parent class use in different classes

--- parents.php
+++ parents.php
@@ -12,6 +12,9 @@
 		 static function modifier_a(){
 			 return self::$a + 10;// self or use class A that means same
 		 }
+		 public function hello(){
+			  echo "<br/> Hello World from A class";
+		 }
 	 }
 	 class B extends A{
 		 static function after_test(){
@@ -20,9 +23,35 @@
 		 static function method_test(){
 			 echo parent::modifier_a();
 		 }
+		 public function instance(){ // create object and access this method
+			 echo parent::hello();
+		 }
+		 public function hello(){
+			 echo "<br/>B class ";
+			 echo parent::hello();
+			 echo "<br/>B class";
+		 }
+		
 	 }
+ 
 	 echo B::$a. "<br/>"
 	 echo B::modifier_a(). "<br/>"
+ 
+	 
+	 
+	 
+	 echo B::$a. "<br/>";
+	 echo B::modifier_a(). "<br/>";
+	 
+	 
+	 echo B::after_test(). "<br/>";
+	 echo B::modifier_a(). "<br/>"; 
+	 
+	 $object = new B();
+	 echo $object-> instance();
+	 echo $object-> hello();
+	 
+ 
 
 	  
 	?>
