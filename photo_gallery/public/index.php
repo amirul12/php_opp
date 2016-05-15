513b79d2a2eaa17cfcfd8fb5686c8d960dc5536d
Amirul Islam <amirul.csejust@gmail.com>
2016-05-15 22:37:41 +0600

    some changes in database and index file to check query

--- photo_gallery/public/index.php
+++ photo_gallery/public/index.php
@@ -1,8 +1,18 @@
 <?php
-
 require_once("../includes/database.php");
-if(isset($database)){echo "true"; } else {echo"false";}
-echo "<br/>";
-echo "Is this workig ?";
+
+if(isset($database)) { echo "true"; } else { echo "false"; }
+echo "<br />";
+
+echo $database->escape_value("It's working?<br />");
+
+$sql  = "INSERT INTO users (id, username, password, first_name, last_name) ";
+$sql .= "VALUES (1, 'amirul', 'amir', 'amirul', 'islam')";
+$result = $database->query($sql);
+
+$sql = "SELECT * FROM users WHERE id = 2";
+$result_set = $database->query($sql);
+$found_user = $database->fetch_array($result_set);
+echo $found_user['username'];
 
 ?>
